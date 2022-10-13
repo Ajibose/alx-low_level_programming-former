203ede704b2fb31d046997e4e0f418739ca5a111
Ajibose <ajiboseibrahim12@gmail.com>
2022-10-13 09:04:43 -0700

    Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef functions_pointers_H
-#define functions_pointer_H
+#define functions_pointers_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
